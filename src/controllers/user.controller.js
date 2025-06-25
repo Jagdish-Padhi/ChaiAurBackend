9f7b3c34b554212c7f6238e01ffa9e19c76c8fc6
Jagdish Padhi <jagdishpadhi9@gmail.com>
2025-06-25 21:30:18 +0530

    feat: add getWatchHistory functionality to retrieve user's watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -449,6 +449,66 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        // _id: req.user._id, NOT CORRECT bcz it will give object id
+        //See subpipeline wala video no.20 time: 7 min
+
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "watch history fetched successfully!"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
